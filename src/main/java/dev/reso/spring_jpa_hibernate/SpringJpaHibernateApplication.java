81520aec32bd93d6a9acfb1c2c84c257442f7110
Diego Reis <diegoreso@me.com>
2025-05-13 18:12:13 -0300

    Project created

---
+++ src/main/java/dev/reso/spring_jpa_hibernate/SpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package dev.reso.spring_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/reso/spring_jpa_hibernate/SpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reso.spring_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
